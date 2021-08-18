343ab6e9265fd4e00a37e13445c7f86a43c7e3a5
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-08-18 21:35:34 +0100

    feat: Import router and switch to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import Calculator from './components/Calculator';
 import React from 'react';
+import Home from './components/Home';
+import Quote from './components/Quote';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-const App = () => <Calculator/>;
-
-export default App;
+ const App = () => {
+    return (
+      <Router>
+          <Header/>
+          <div className="Section">
+          <Switch>
+            <Route exact path="/Home">
+              <Home />
+            </Route>
+            <Route path="/Calculator">
+              <Calculator />
+            </Route>
+            <Route path="/Quote">
+              <Quote />
+            </Route>
+          </Switch>
+        </div>
+        <Footer/>
+      </Router>
+    );
+  }
+  export default App;
