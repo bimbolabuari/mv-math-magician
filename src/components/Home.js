1675a3eabf5759282800acebc8edd581a1bd9b6b
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-08-18 21:37:32 +0100

    feat: Create home page for web page

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+ const Home = () => {
+    return (
+        <div>
+            <h2 className="homeTitle">Welcome to our page</h2>
+            <p className="homeParagraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id nisi erat. Suspendisse convallis erat non orci auctor, ut venenatis elit tincidunt.
+             <br/>Ut non augue non metus interdum ultricies vel quis felis. Vivamus vitae sapien ut elit blandit faucibus. Nulla a nunc eu lacus mollis congue. <br/>Nulla facilisi. Nam porttitor tellus vestibulum sem sodales hendrerit. Quisque pulvinar urna vitae nisi dignissim, in lacinia urna laoreet.</p>
+        </div>
+    )
+}
+
+export default Home;
